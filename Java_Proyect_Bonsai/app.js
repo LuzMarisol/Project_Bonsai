41e9b6e768685bdf230d8fd0d7d4a9d8e14d9fde
Luz Marisol <43183203+LuzMarisol@users.noreply.github.com>
2020-12-20 23:49:55 -0600

    Add files via upload

---
+++ Java_Proyect_Bonsai/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const { API_VERSION } = require("./config");
+
+// Load routings
+const messageRoutes = require("./routers/message");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configure Header HTTP
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+// Router Basic
+app.use(`/api/${API_VERSION}`, messageRoutes);
+
+module.exports = app;
